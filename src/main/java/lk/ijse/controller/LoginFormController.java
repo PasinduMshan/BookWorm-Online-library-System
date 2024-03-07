332903126cc09db482d2d0b0ed2aeb9399a2e4a0
pasindu <madushan01722@gmail.com>
2024-03-07 18:20:15 +0530

    create login form UI

---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,31 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class LoginFormController {
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnViewPasswordOnAction(MouseEvent event) {
+
+    }
+
+}
