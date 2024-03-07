cc434d2fa7548478400c9d36245193bad1b42beb
pasindu <madushan01722@gmail.com>
2024-03-07 18:38:34 +0530

    create signup form UI

---
+++ src/main/java/lk/ijse/controller/SignUpFormController.java
@@ -0,0 +1,30 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SignUpFormController {
+
+    @FXML
+    private JFXPasswordField txtConfirmPassword;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+}
