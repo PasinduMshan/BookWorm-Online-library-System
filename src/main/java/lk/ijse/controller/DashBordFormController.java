cc6f821f8e6729f60143e8bed81ffebedbaefd91
pasindu <madushan01722@gmail.com>
2024-03-11 19:30:48 +0530

    create DashBoard form UI

---
+++ src/main/java/lk/ijse/controller/DashBordFormController.java
@@ -0,0 +1,43 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashBordFormController {
+
+    @FXML
+    void btnAdminOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBorrowReturnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashBoardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMembersOnAction(ActionEvent event) {
+
+    }
+
+}
